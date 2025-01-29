f2b7e6880a2635586733faf73b20708b592a6e60
BlackOwl242 <141981096+BlackOwl242@users.noreply.github.com>
2025-01-30 00:13:27 +0700

    Update UI 1.1

--- scripts.js
+++ scripts.js
@@ -7,18 +7,58 @@ window.addEventListener('scroll', function() {
     const sectionTop = rect.top + scrollTop;
     const sectionHeight = rect.height;
 
-    // Tính toán tỷ lệ cuộn
     const scrollRatio = (scrollTop + windowHeight - sectionTop) / (sectionHeight);
 
-    // Giới hạn tỷ lệ cuộn trong khoảng từ 0 đến 1
-    const clampedScrollRatio = Math.min(Math.max(scrollRatio, 0), 1);
+     // Giới hạn tỷ lệ cuộn trong khoảng từ 0 đến 1
+     const clampedScrollRatio = Math.min(Math.max(scrollRatio, 0), 1);
 
-    // Tính toán vị trí mới của phần tử
-    const newLeft = -100 + (100 * clampedScrollRatio); // Từ -100% đến 0%
-    const newRotation = 360 * clampedScrollRatio; // Từ 0deg đến 360deg
-    requestAnimationFrame(() => {
-        img.style.left = `${newLeft}%`;
-        img.style.transform = `translateX(-40%)`;
-        img.style.transform = `translateX(${newLeft}%) rotate(${newRotation}deg)`;
+     // Tính toán vị trí mới của phần tử
+     const newLeft = -100 + (100 * clampedScrollRatio); // Từ -100% đến 0%
+     const newRotation = 360 * clampedScrollRatio; // Từ 0deg đến 360deg
+     requestAnimationFrame(() => {
+         img.style.left = `${newLeft}%`;
+         img.style.transform = `translateX(-40%)`;
+         img.style.transform = `translateX(${newLeft}%) rotate(${newRotation}deg)`;
+        });
+});
+
+window.addEventListener('load', function() {
+     const loadingScreen = document.getElementsByClassName('spinner')[0];
+     loadingScreen.style.display = 'none';
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var footer = document.getElementById("footer");
+    var sentinel = document.querySelector(".sentinel");
+
+    var observer = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting) {
+            footer.classList.add("show");
+        } else {
+            footer.classList.remove("show");
+        }
     });
+
+    observer.observe(sentinel);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var allColumns = document.querySelector(".allColumns");
+
+    var observer = new IntersectionObserver(function(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                var ratio = entry.intersectionRatio;
+                if (ratio >= 0.5) {
+                    allColumns.style.opacity = 1; // Fully visible
+                } else {
+                    allColumns.style.opacity = ratio * 2; // Partially visible
+                }
+            } else {
+                allColumns.style.opacity = 0; // Not visible
+            }
+        });
+    }, { threshold: [0, 0.5, 1] });
+
+    observer.observe(allColumns);
 });
